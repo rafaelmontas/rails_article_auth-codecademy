2bc8b440e5c359b4b06b4cb5beb2af1f1d485ece
Rafael Montas <rafaelmontas1@gmail.com>
2016-10-12 11:39:36 -0400

    Edit user info ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	before_action :require_logged_out, only: [:new]
-	before_action :require_user, only: [:show]
+	before_action :require_user, only: [:show, :edit]
 
 	def show
 		@user = User.find(session[:user_id])
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def edit
+		@user = User.find(session[:user_id])
+	end
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
@@ -17,6 +21,15 @@ class UsersController < ApplicationController
 			redirect_to "/"
 		else
 			redirect_to "/signup"
+		end
+	end
+
+	def update
+		@user = User.find(session[:user_id])
+		if @user.update(user_params)
+			redirect_to @user
+		else
+			render "edit"
 		end
 	end
 
