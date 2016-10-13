1f6af0e3041fb220783d0d76763bbfdaf93e7ef1
Rafael Montas <rafaelmontas1@gmail.com>
2016-10-13 18:49:23 -0400

    Adding creation and edition of articles and user creation with role

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-	before_action :require_user, only: [:show]
+	before_action :require_user, only: [:show, :new]
 	def index
 		@articles = Article.all
 	end
@@ -7,4 +7,36 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def new 
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to @article
+		else
+			render "new"
+		end
+	end	
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render "edit"
+		end
+
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :author, :body)
+	end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,6 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :email, :password)
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :role)
 	end
 end
