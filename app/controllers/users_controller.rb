d165671f36a01480f2a2444812d190f68e00520a
Rafael Montas <rafaelmontas1@gmail.com>
2016-10-14 13:07:46 -0400

    Users can attach images to their profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,6 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :email, :password, :role)
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :role, :image)
 	end
 end
