ea7275871d89c24fa27a7e83ccd997853f855794
Rafael Montas <rafaelmontas1@gmail.com>
2016-10-14 12:32:36 -0400

    User model edited (paperclip), image attachment added and add image deleted(db:migrate)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
 	has_secure_password
 
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100#" }
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+
 	def author?
 		self.role == "author"
 	end
